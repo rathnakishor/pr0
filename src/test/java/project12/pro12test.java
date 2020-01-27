9ff94465a6f0bafd1e5b9864f81df3429aaba21e
Your Name <you@example.com>
2020-01-27 18:30:24 +0530

    1st

---
+++ src/test/java/project12/pro12test.java
@@ -0,0 +1,14 @@
+package project12;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class pro12test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
